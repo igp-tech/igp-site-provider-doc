0fe25adca1ebb824e0c6d00efc7d03bd8f887661
Domagoj <dsitum@aut-aut.hr>
2020-02-14 11:59:42 +0100

    Latest site provider updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
 var gulp = require('gulp');
-var util = require('gulp-util')
 var gulpConnect = require('gulp-connect');
-var connect = require('connect');
+var express = require('express');
 var cors = require('cors');
 var path = require('path');
 var exec = require('child_process').exec;
 var portfinder = require('portfinder');
 var swaggerRepo = require('swagger-repo');
+var colors = require('colors/safe');
 
 var DIST_DIR = 'web_deploy';
 
-gulp.task('serve', ['build', 'watch', 'edit'], function() {
+function serve(cb) {
   portfinder.getPort({port: 3000}, function (err, port) {
     gulpConnect.server({
       root: [DIST_DIR],
@@ -23,28 +23,32 @@ gulp.task('serve', ['build', 'watch', 'edit'], function() {
       }
     });
   });
-});
+  cb();
+}
 
-gulp.task('edit', function() {
-  portfinder.getPort({port: 5000}, function (err, port) {
-    var app = connect();
-    app.use(swaggerRepo.swaggerEditorMiddleware());
-    app.listen(port);
-    util.log(util.colors.green('swagger-editor started http://localhost:' + port));
-  });
-});
+function edit(cb) {
+  app = express();
+  app.listen(5000, () => colors.green('swagger-editor started http://localhost:5000'));
+  app.use(swaggerRepo.swaggerEditorMiddleware());
+  cb();
+}
 
-gulp.task('build', function (cb) {
+function build(cb) {
   exec('npm run build', function (err, stdout, stderr) {
     console.log(stderr);
     cb(err);
   });
-});
+}
 
-gulp.task('reload', ['build'], function () {
-  gulp.src(DIST_DIR).pipe(gulpConnect.reload())
-});
+function reload(cb) {
+  gulp.src(DIST_DIR).pipe(gulpConnect.reload());
+  cb();
+}
 
-gulp.task('watch', function () {
-  gulp.watch(['spec/**/*', 'web/**/*'], ['reload']);
-});
+function watch(cb) {
+  gulp.watch(['spec/**/*', 'web/**/*'], gulp.series(build, reload));
+  cb();
+}
+
+exports.default = gulp.series(build, watch, edit, serve);
+exports.serve = serve;
